99f0ece1e72baa72b09aae211a993cb5e1ee4da9
scrabill <shannon.crabill@gmail.com>
2020-02-02 21:59:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your functions here
+
+// function whomp(array) {
+//   return array * -1
+// }
+
+function map(array, callback) {
+
+  let newArray = []
+
+  array.forEach((item, i) => {
+    newArray.push(callback(item))
+  });
+
+  return newArray
+}
+
+function reduce(array, startingValue) {
+
+  let total = 0
+
+  if (!!startingValue) {
+    total = startingValue
+  }
+
+  array.forEach((item, i) => {
+    total = total + item
+  });
+
+  return total
+
+}
